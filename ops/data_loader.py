Fixed SVM implementation by adding normalization to CNN scores.

ops/data_loader.py
@@ -213,7 +213,10 @@ def read_and_decode(
 
     # Convert label from a scalar uint8 tensor to an int32 scalar.
     label = tf.cast(features[label_key.split('/')[0]], tf.int32)
-    return image, label
+
+    # Get filename as a string.
+    filename = tf.cast(features[filename_key.split('/')[0]], tf.string)
+    return image, label, filename
 
 
 def inputs(
@@ -240,14 +243,14 @@ def inputs(
         # (Internally uses a RandomShuffleQueue.)
         # We run this in two threads to avoid being a bottleneck.
         if shuffle_batch:
-            images, labels = tf.train.shuffle_batch(
+            images, labels, filenames = tf.train.shuffle_batch(
                 batch_data, batch_size=batch_size, num_threads=num_threads,
                 capacity=1000 + 3 * batch_size,
                 # Ensures a minimum amount of shuffling of examples.
                 min_after_dequeue=1000)
         else:
-            images, labels = tf.train.batch(
+            images, labels, filenames = tf.train.batch(
                 batch_data, batch_size=batch_size, num_threads=num_threads,
                 capacity=1000 + 3 * batch_size)
 
-        return images, labels
+        return images, labels, filenames

ops/modeling_sklearn.py
@@ -3,11 +3,12 @@ import re
 import time
 import numpy as np
 import tensorflow as tf
+import cPickle
 from datetime import datetime
 from ops import utilities
 from ops.data_loader import inputs
 from ops import tf_loss
-
+from sklearn import svm
 
 def print_status(step, loss_value, config, duration, validation_accuracy, log_dir):
     format_str = (
@@ -29,18 +30,13 @@ def import_cnn(model_type):
         __import__('models', fromlist=[model_type]), model_type)
 
 
-def batchnorm(layer):
-    m, v = tf.nn.moments(layer, [0])
-    return tf.nn.batch_normalization(layer, m, v, None, None, 1e-3)
-
-
 def choose_classifier(sample_layer, y, config):
     if config.classifier == 'softmax':
         weights, preds = build_softmax(sample_layer)
-        classifier, loss = softmax_optimization(yhat=preds, y=y, W=weights, b=b, c=config.c, lr=config.lr)
+        classifier, loss = softmax_optimization(preds, y, weights, config.c, config.lr)
     elif config.classifier == 'svm':
-        weights, bias, preds = build_svm(sample_layer)
-        classifier, loss = svm_optimization(yhat=preds, y=y, W=weights, b=bias, c=config.c, lr=config.lr)
+        weights, preds = build_svm(sample_layer)
+        classifier, loss = svm_optimization(preds, y, weights, config.c, config.lr)
     print 'Using a %s' % config.classifier
     return weights, preds, classifier, loss
 
@@ -51,29 +47,38 @@ def build_softmax(x):
         'softmax_W', initializer=tf.zeros([features, 1]))
     b = tf.get_variable(
         'softmax_b', initializer=tf.zeros([1]))
-    return W, b, tf.nn.bias_add(tf.matmul(x, W), b)
+    return W, tf.nn.bias_add(tf.matmul(x, W), b)
 
 
-def softmax_optimization(yhat, y, W, b, c=0.01, lr=0.01):
+def softmax_optimization(yhat, y, W, c=0.01, lr=0.01):
     class_loss = tf.reduce_mean(
         tf.nn.sparse_softmax_cross_entropy_with_logits(yhat, y))
     class_loss += (tf.nn.l2_loss(W) * c)  # l2 regularization
-    return tf.train.GradientDescentOptimizer(lr).minimize(class_loss, var_list=[W, b]), class_loss
+    return tf_loss.finetune_learning(
+        loss=class_loss,
+        trainable_variables=tf.trainable_variables(),
+        lr=lr,
+        fine_tune_layers=['softmax_W', 'softmax_b'],
+        optimizer='adam'), class_loss
 
 
 def build_svm(x):
     features = int(x.get_shape()[-1])
     W = tf.get_variable('svm_W', initializer=tf.zeros([features, 1]))
     b = tf.get_variable('svm_b', initializer=tf.zeros([1]))
-    bnx = batchnorm(x)
-    return W, b, tf.nn.bias_add(tf.matmul(bnx, W), b)
+    return W, tf.matmul(x, W) + b
 
 
-def svm_optimization(yhat, y, W, b, c=1, lr=0.01):
+def svm_optimization(yhat, y, W, c=1, lr=0.01):
     regularization_loss = 0.5*tf.reduce_sum(tf.square(W))
     class_loss = tf_loss.hinge_loss(yhat, y)
     svm_loss = regularization_loss + c*class_loss
-    return tf.train.GradientDescentOptimizer(lr).minimize(svm_loss, var_list=[W, b]), svm_loss
+    return tf_loss.finetune_learning(
+        loss=svm_loss,
+        trainable_variables=tf.trainable_variables(),
+        lr=lr,
+        fine_tune_layers=['svm_W', 'svm_b'],
+        optimizer='adam'), svm_loss
 
 
 def train_classifier_on_model(
@@ -121,10 +126,6 @@ def train_classifier_on_model(
             cnn.build(
                 train_images)
             sample_layer = cnn[selected_layer]  # sample features here with a mask: self.number_of_features
-            weights, yhat, classifier, class_loss = choose_classifier(
-                sample_layer=sample_layer,
-                y=train_labels,
-                config=config)
 
     saver = tf.train.Saver(
         tf.all_variables(), max_to_keep=10)
@@ -142,29 +143,29 @@ def train_classifier_on_model(
     np.save(
         os.path.join(
             config.checkpoint_directory, 'training_config_file'), config)
-    step, losses = 0, []
+    step, scores, labs = 0, [], []
     try:
-        print 'Training model'
+        print 'Getting scores'
         while not coord.should_stop():
             start_time = time.time()
-            _, loss_value, images, labels = sess.run(
-                [classifier, class_loss, train_images, train_labels])
-            losses.append(loss_value)
+            score, lab = sess.run(
+                [sample_layer, train_labels])
+            scores += [score]
+            labs += [lab]
             duration = time.time() - start_time
-            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
             # End iteration
-            print_status(step, loss_value, config, duration, 0, '')
+            print_status(step, 1, config, duration, 1, '')
             step += 1
-
     except tf.errors.OutOfRangeError:
-        print 'Done training for %d epochs, %d steps.' % (config.epochs, step)
+        import ipdb;ipdb.set_trace()
+        svc = svm.LinearSVC(C=config.c, verbose=True).fit(np.asarray(scores), np.asarray(labs))
         print 'Saved to: %s' % config.checkpoint_directory
     finally:
         ckpt_path = os.path.join(
                 config.checkpoint_directory,
-                'model_' + str(step) + '.ckpt')
-        saver.save(
-            sess, ckpt_path, global_step=step)
+                'model_' + str(step) + '.pkl')
+        with open(ckpt_path, 'wb') as fid:
+            cPickle.dump(svc, fid)    
         print 'Saved checkpoint to: %s' % ckpt_path
         coord.request_stop()
     # Return the final checkpoint for testing

ops/testing.py
@@ -29,18 +29,13 @@ def import_cnn(model_type):
         __import__('models', fromlist=[model_type]), model_type)
 
 
-def batchnorm(layer):
-    m, v = tf.nn.moments(layer, [0])
-    return tf.nn.batch_normalization(layer, m, v, None, None, 1e-3)
-
-
 def choose_classifier(sample_layer, y, config):
     if config.classifier == 'softmax':
         weights, preds = build_softmax(sample_layer)
-        classifier, loss = softmax_optimization(yhat=preds, y=y, W=weights, b=b, c=config.c, lr=config.lr)
+        classifier, loss = softmax_optimization(preds, y, weights, config.c, config.lr)
     elif config.classifier == 'svm':
-        weights, bias, preds = build_svm(sample_layer)
-        classifier, loss = svm_optimization(yhat=preds, y=y, W=weights, b=bias, c=config.c, lr=config.lr)
+        weights, preds = build_svm(sample_layer)
+        classifier, loss = svm_optimization(preds, y, weights, config.c, config.lr)
     print 'Using a %s' % config.classifier
     return weights, preds, classifier, loss
 
@@ -48,70 +43,69 @@ def choose_classifier(sample_layer, y, config):
 def build_softmax(x):
     features = int(x.get_shape()[-1])
     W = tf.get_variable(
-        'softmax_W', initializer=tf.zeros([features, 1]))
+        'softmax_W', initializer=tf.truncated_normal([features, 1], 0.0, 0.001))
     b = tf.get_variable(
-        'softmax_b', initializer=tf.zeros([1]))
-    return W, b, tf.nn.bias_add(tf.matmul(x, W), b)
+        'softmax_b', initializer=tf.truncated_normal([1], 0.0, 0.001))
+    return W, tf.nn.bias_add(tf.matmul(x, W), b)
 
 
-def softmax_optimization(yhat, y, W, b, c=0.01, lr=0.01):
+def softmax_optimization(yhat, y, W, c=0.01, lr=0.01):
     class_loss = tf.reduce_mean(
         tf.nn.sparse_softmax_cross_entropy_with_logits(yhat, y))
     class_loss += (tf.nn.l2_loss(W) * c)  # l2 regularization
-    return tf.train.GradientDescentOptimizer(lr).minimize(class_loss, var_list=[W, b]), class_loss
+    return tf_loss.finetune_learning(
+        loss=class_loss,
+        trainable_variables=tf.trainable_variables(),
+        lr=0,
+        fine_tune_layers=['softmax_W', 'softmax_b'],
+        optimizer='adam'), class_loss
 
 
 def build_svm(x):
     features = int(x.get_shape()[-1])
     W = tf.get_variable('svm_W', initializer=tf.zeros([features, 1]))
     b = tf.get_variable('svm_b', initializer=tf.zeros([1]))
-    bnx = batchnorm(x)
-    return W, b, tf.nn.bias_add(tf.matmul(bnx, W), b)
+    return W, tf.matmul(x, W) + b
 
 
-def svm_optimization(yhat, y, W, b, c=1, lr=0.01):
+def svm_optimization(yhat, y, W, c=1, lr=0.01):
     regularization_loss = 0.5*tf.reduce_sum(tf.square(W))
     class_loss = tf_loss.hinge_loss(yhat, y)
     svm_loss = regularization_loss + c*class_loss
-    return tf.train.GradientDescentOptimizer(lr).minimize(svm_loss, var_list=[W, b]), svm_loss
+    return tf_loss.finetune_learning(
+        loss=svm_loss,
+        trainable_variables=tf.trainable_variables(),
+        lr=0,
+        fine_tune_layers=['svm_W', 'svm_b'],
+        optimizer='adam'), svm_loss
 
 
-def train_classifier_on_model(
-        train_pointer,
+def test_classifier(
+        validation_pointer,
+        model_ckpt,
+        model_dir,
         model_type,
         model_weights,
         selected_layer,
         config):
 
     # Make output directories if they do not exist
-    dt_stamp = '%s_%s_%s_%s' % (
-        model_type,
-        selected_layer,
-        str(config.lr)[2:],
-        re.split(
-            '\.', str(datetime.now(
-                )))[0].replace(' ', '_').replace(':', '_').replace('-', '_')
-        )
-    config.checkpoint_directory = os.path.join(
-        config.checkpoint_directory, dt_stamp)  # timestamp this run
-    dir_list = [config.checkpoint_directory]
-    [utilities.make_dir(d) for d in dir_list]
+    config.checkpoint_directory = model_dir
 
     print '-'*60
-    print'Training %s over a %s. Saving to %s' % (config.classifier, model_type, dt_stamp)
+    print'Testing the model over a %s. Saving to %s' % (model_type, model_dir)
     print '-'*60
 
     dcn_flavor = import_cnn(model_type)
 
     # Prepare data on CPU
     with tf.device('/cpu:0'):
-        train_images, train_labels, train_files = inputs(
-            train_pointer,
-            config.train_batch,
-            config.train_image_size,
-            config.model_image_size[:2],
-            num_epochs=config.epochs,
-            shuffle_batch=True)
+        val_images, val_labels, val_files = inputs(
+            validation_pointer,
+            config.validation_batch,
+            config.validation_image_size,
+            config.model_image_size[:2],1,
+            shuffle_batch=False)
 
     # Prepare pretrained model on GPU
     with tf.device('/gpu:0'):
@@ -119,16 +113,17 @@ def train_classifier_on_model(
             cnn = dcn_flavor.model(
                 weight_path=model_weights)
             cnn.build(
-                train_images)
+                val_images)
             sample_layer = cnn[selected_layer]  # sample features here with a mask: self.number_of_features
             weights, yhat, classifier, class_loss = choose_classifier(
                 sample_layer=sample_layer,
-                y=train_labels,
+                y=val_labels,
                 config=config)
+            # Calculate validation accuracy
+            val_accuracy = tf_loss.class_accuracy(tf.nn.relu(tf.sign(yhat)), val_labels)
 
     saver = tf.train.Saver(
         tf.all_variables(), max_to_keep=10)
-
     # Initialize the graph
     sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
     # Need to initialize both of these if supplying num_epochs to inputs
@@ -139,34 +134,39 @@ def train_classifier_on_model(
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
     # Start training loop
-    np.save(
-        os.path.join(
-            config.checkpoint_directory, 'training_config_file'), config)
-    step, losses = 0, []
+    results = {
+        'accs': [],
+        'preds': [],
+        'labs': [],
+        'files': []
+    }
+    if int(tf.__version__.split('.')[1]) > 10:
+        model_ckpt += '-%s' % re.search('\d+.ckpt', model_ckpt).group().split('.ckpt')[0]
+    saver.restore(sess, model_ckpt)
+    np_path = os.path.join(
+        config.checkpoint_directory, 'validation_results')
+    step = 0
     try:
-        print 'Training model'
+        print 'Testing model'
         while not coord.should_stop():
             start_time = time.time()
-            _, loss_value, images, labels = sess.run(
-                [classifier, class_loss, train_images, train_labels])
-            losses.append(loss_value)
+            acc, pred, lab, f = sess.run(
+                [val_accuracy, yhat, val_labels, val_files])
+            import ipdb;ipdb.set_trace()
+            results['accs'] += [acc]
+            results['preds'] += [pred]
+            results['labs'] += [lab]
+            results['files'] += [f]
             duration = time.time() - start_time
-            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
-            # End iteration
-            print_status(step, loss_value, config, duration, 0, '')
+            print_status(step, 0, config, duration, acc, np_path)
             step += 1
 
     except tf.errors.OutOfRangeError:
-        print 'Done training for %d epochs, %d steps.' % (config.epochs, step)
-        print 'Saved to: %s' % config.checkpoint_directory
+        print 'Done testing.'
     finally:
-        ckpt_path = os.path.join(
-                config.checkpoint_directory,
-                'model_' + str(step) + '.ckpt')
-        saver.save(
-            sess, ckpt_path, global_step=step)
-        print 'Saved checkpoint to: %s' % ckpt_path
+        np.savez(np_path, **results)
+        print 'Saved to: %s' % np_path
         coord.request_stop()
-    # Return the final checkpoint for testing
-    return ckpt_path, config.checkpoint_directory
-
+    coord.join(threads)
+    sess.close()
+    print '%.4f%% correct' % np.mean(acc)

ops/tf_loss.py
@@ -20,7 +20,7 @@ def finetune_learning(
         fine_tune_layers=None,
         optimizer='adam'):
     if fine_tune_layers is not None:
-        hold_lr = lr * 3
+        hold_lr = lr / 3
         other_opt_vars, ft_opt_vars = fine_tune_prepare_layers(
             trainable_variables, fine_tune_layers)
         if optimizer == 'adam':
@@ -69,5 +69,5 @@ def class_accuracy(pred, targets):
 
 def hinge_loss(yhat, y):
     return tf.reduce_mean(tf.nn.relu(
-        1 - tf.squeeze(yhat) * tf.cast(y, tf.float32)))
+        1 - (tf.squeeze(yhat) * tf.cast(y, tf.float32))))
     # return tf.contrib.losses.hinge_loss(tf.squeeze(yhat), tf.cast(y, tf.float32))

ops/utilities.py
@@ -25,10 +25,17 @@ def make_dir(d):
         os.makedirs(d)
 
 
-def resample_image_lists(it_list, sp):
+def resample_image_lists(im_list, lab_list, sp):
     # Shuffle each it_list then sample sp
-    sampled_list = []
-    for il, itsp in zip(it_list, sp):
-        npil = np.asarray(il)[np.random.permutation(len(il))]
-        sampled_list += [list(npil[:itsp])]
-    return sampled_list
+    sampled_ims = []
+    sampled_labs = []
+    for im, il, itsp in zip(im_list, lab_list, sp):
+        npim = np.asarray(im)
+        npil = np.asarray(il)
+        shuffle = np.random.permutation(len(im))
+        sampled_ims += [list(npim[shuffle[:itsp]])]
+        sampled_labs += [list(npil[shuffle[:itsp]])]
+    return sampled_ims, sampled_labs
+
+def shuffle_list(l):
+    return list(np.asarray(l)[np.random.permutation(len(l))])