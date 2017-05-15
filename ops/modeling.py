50713e9964fcce8981ec7facd475a0874d79018f
Drew Linsley <drewlinsley@gmail.com>
2017-05-15 14:59:17 -0400

    Factored out svm code to tf_loss.

--- ops/modeling.py
+++ ops/modeling.py
@@ -10,7 +10,7 @@ from ops import tf_loss
 
 
 def print_status(
-    step, loss_value, config, duration, validation_accuracy, log_dir):
+        step, loss_value, config, duration, validation_accuracy, log_dir):
     format_str = (
         '%s: step %d, loss = %.2f (%.1f examples/sec; '
         '%.3f sec/batch) | validation accuracy %.3f | logdir = %s')
@@ -28,57 +28,6 @@ def print_status(
 def import_cnn(model_type):
     return getattr(
         __import__('models', fromlist=[model_type]), model_type)
-
-
-def batchnorm(layer):
-    m, v = tf.nn.moments(layer, [0])
-    return tf.nn.batch_normalization(layer, m, v, None, None, 1e-3)
-
-
-def choose_classifier(sample_layer, y, config):
-    if config.classifier == 'softmax':
-        weights, bias, preds = build_softmax(sample_layer)
-        classifier, loss = softmax_optimization(
-            yhat=preds, y=y, W=weights, b=bias, c=config.c, lr=config.lr)
-    elif config.classifier == 'svm':
-        weights, bias, preds = build_svm(sample_layer)
-        classifier, loss = svm_optimization(
-            yhat=preds, y=y, W=weights, b=bias, c=config.c, lr=config.lr)
-    print 'Using a %s' % config.classifier
-    return weights, preds, classifier, loss
-
-
-def build_softmax(x):
-    features = int(x.get_shape()[-1])
-    W = tf.get_variable(
-        'softmax_W', initializer=tf.zeros([features, 1]))
-    b = tf.get_variable(
-        'softmax_b', initializer=tf.zeros([1]))
-    return W, b, tf.nn.bias_add(tf.matmul(x, W), b)
-
-
-def softmax_optimization(yhat, y, W, b, c=0.01, lr=0.01):
-    class_loss = tf.reduce_mean(
-        tf.nn.sparse_softmax_cross_entropy_with_logits(yhat, y))
-    class_loss += (tf.nn.l2_loss(W) * c)  # l2 regularization
-    return tf.train.GradientDescentOptimizer(lr).minimize(
-        class_loss, var_list=[W, b]), class_loss
-
-
-def build_svm(x):
-    features = int(x.get_shape()[-1])
-    W = tf.get_variable('svm_W', initializer=tf.zeros([features, 1]))
-    b = tf.get_variable('svm_b', initializer=tf.zeros([1]))
-    bnx = batchnorm(x)
-    return W, b, tf.nn.bias_add(tf.matmul(bnx, W), b)
-
-
-def svm_optimization(yhat, y, W, b, c=1, lr=0.01):
-    regularization_loss = 0.5*tf.reduce_sum(tf.square(W))
-    class_loss = tf_loss.hinge_loss(yhat, y)
-    svm_loss = regularization_loss + c*class_loss
-    return tf.train.GradientDescentOptimizer(lr).minimize(
-        svm_loss, var_list=[W, b]), svm_loss
 
 
 def train_classifier_on_model(
@@ -130,7 +79,7 @@ def train_classifier_on_model(
             cnn.build(
                 train_images)
             sample_layer = cnn[selected_layer]
-            weights, yhat, classifier, class_loss = choose_classifier(
+            weights, yhat, classifier, class_loss = tf_loss.choose_classifier(
                 sample_layer=sample_layer,
                 y=train_labels,
                 config=config)
--- ops/tf_loss.py
+++ ops/tf_loss.py
@@ -71,3 +71,55 @@ def hinge_loss(yhat, y):
     return tf.reduce_mean(tf.nn.relu(
         1 - (tf.squeeze(yhat) * tf.cast(y, tf.float32))))
     # return tf.contrib.losses.hinge_loss(tf.squeeze(yhat), tf.cast(y, tf.float32))
+
+
+def batchnorm(layer):
+    m, v = tf.nn.moments(layer, [0])
+    return tf.nn.batch_normalization(layer, m, v, None, None, 1e-3)
+
+
+def choose_classifier(sample_layer, y, config):
+    if config.classifier == 'softmax':
+        weights, bias, preds = build_softmax(sample_layer)
+        classifier, loss = softmax_optimization(
+            yhat=preds, y=y, W=weights, b=bias, c=config.c, lr=config.lr)
+    elif config.classifier == 'svm':
+        weights, bias, preds = build_svm(sample_layer)
+        classifier, loss = svm_optimization(
+            yhat=preds, y=y, W=weights, b=bias, c=config.c, lr=config.lr)
+    print 'Using a %s' % config.classifier
+    return weights, preds, classifier, loss
+
+
+def build_softmax(x):
+    features = int(x.get_shape()[-1])
+    W = tf.get_variable(
+        'softmax_W', initializer=tf.zeros([features, 1]))
+    b = tf.get_variable(
+        'softmax_b', initializer=tf.zeros([1]))
+    return W, b, tf.nn.bias_add(tf.matmul(x, W), b)
+
+
+def softmax_optimization(yhat, y, W, b, c=0.01, lr=0.01):
+    class_loss = tf.reduce_mean(
+        tf.nn.sparse_softmax_cross_entropy_with_logits(yhat, y))
+    class_loss += (tf.nn.l2_loss(W) * c)  # l2 regularization
+    return tf.train.GradientDescentOptimizer(lr).minimize(
+        class_loss, var_list=[W, b]), class_loss
+
+
+def build_svm(x):
+    features = int(x.get_shape()[-1])
+    W = tf.get_variable('svm_W', initializer=tf.zeros([features, 1]))
+    b = tf.get_variable('svm_b', initializer=tf.zeros([1]))
+    bnx = batchnorm(x)
+    return W, b, tf.nn.bias_add(tf.matmul(bnx, W), b)
+
+
+def svm_optimization(yhat, y, W, b, c=1, lr=0.01):
+    regularization_loss = 0.5*tf.reduce_sum(tf.square(W))
+    class_loss = hinge_loss(yhat, y)
+    svm_loss = regularization_loss + c*class_loss
+    return tf.train.GradientDescentOptimizer(lr).minimize(
+        svm_loss, var_list=[W, b]), svm_loss
+
